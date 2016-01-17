1806b11f8ab4834d398c8cd8e556c891c2e2b318
Alan O'Connor <alanoconnor@Alans-MacBook-Pro.local>
2016-01-17 20:58:14 +0000

    Start of swift closures

--- GettingStartedSwift/GettingStartedSwift/SwiftClosures.swift
+++ GettingStartedSwift/GettingStartedSwift/SwiftClosures.swift
@@ -9,5 +9,18 @@
 import Foundation
 
 class SwiftClosures {
+    /**
+    *  CLosures are self contained blocks of functionality that can be passed around and used in your code. 
+    Closures in swift are similar to blocks in C and objective-C
     
+    Global and nested functions are special types of closures
+    
+    Global functions are closures that have a name and do not capture any values
+    
+    Nested Functions are closures that have a name and can capture values from their enclosing function
+    
+    Closure Expressions are unnamed closures written in a lightweight syntax that can capture
+    values from their surrounding context
+
+    */
 }
