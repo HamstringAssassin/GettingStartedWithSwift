486cedc524693b0092b73dbf61108d4208ffd226
Alan O'Connor <alan@getdrop.com>
2016-11-23 11:02:09 +0000

    Update

--- GettingStartedSwift/GettingStartedSwift/ArraysAndDictionaries.swift
+++ GettingStartedSwift/GettingStartedSwift/ArraysAndDictionaries.swift
@@ -9,31 +9,60 @@
 import Foundation
 
 class ArraysAndDictionaries {
-	
-	
-	func testArraysAndDictionaries() {
-		let arraysAndDic = ArraysAndDictionaries()
-		arraysAndDic.basicDictionariesAndArrays()
-	}
-	
-	
-	func basicDictionariesAndArrays(){
-		// Creating a dictionary
-		var occupations = [
-			"malcolm" : "captain",
-			"Kaylee" : "Mechanic",
-		]
-		
-		occupations["Jane"] = "Public Relations"
-		
-		//Creating empty arrays or dictionaries
-		
-		let emptyArray = [String]()
-		let emptyDictionary = [String:Float]()
-		
-		// this information can also be infered. eg
-		let optionalEmptyArray = []
-		let optionalEmptyDictionary = [:]
-		
-	}	
+    
+    
+    func testArraysAndDictionaries() {
+        let arraysAndDic = ArraysAndDictionaries()
+        arraysAndDic.basicDictionariesAndArrays()
+        arraysAndDic.testMappingArray()
+        arraysAndDic.testFilterArray()
+    }
+    
+    
+    func basicDictionariesAndArrays(){
+        // Creating a dictionary
+        var occupations = [
+            "malcolm" : "captain",
+            "Kaylee" : "Mechanic",
+            ]
+        
+        occupations["Jane"] = "Public Relations"
+        
+        //Creating empty arrays or dictionaries
+        
+        let emptyArray = [String]()
+        let emptyDictionary = [String:Float]()
+        
+        // this information can also be infered. eg
+        let optionalEmptyArray = []
+        let optionalEmptyDictionary = [:]
+        
+    }
+    
+    func testMappingArray() {
+        let testArray = [1,2,3,4,5,6,7,8,9]
+        let newArray = testArray
+            .map {
+                return $0*2
+            }
+        print(newArray)
+    }
+    
+    func testFilterArray() {
+        let testArray = [1,2,3,4,5,6,7,8,9]
+        let result = testArray
+        .filter { (value) in
+            return value % 2 == 0
+        }
+        print(result)
+        
+        //alternative form 
+        let result2 = testArray
+            .filter {
+                return $0 % 2 == 0
+        }
+        print(result2)
+    }
+    
+    
 }
